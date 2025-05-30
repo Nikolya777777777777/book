28e41e328ad4aa5a7016acbb1f4b3a0bc701f252
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2025-05-30 19:40:33 +0200

    added CustomGlobalExceptionHandler and data validation

--- src/main/java/com/example/demo/dto/BookDto.java
+++ src/main/java/com/example/demo/dto/BookDto.java
@@ -1,14 +1,23 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookDto {
+    @NotNull
+    @Min(0)
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
     private String isbn;
     private String description;
     private String coverImage;
