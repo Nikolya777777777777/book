94aad0579ac479f1b30f36fcf15ccf9ca1eb998f
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2025-06-22 10:47:04 +0200

    only start of security configuration

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import com.example.demo.model.enums.RoleName;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+}
